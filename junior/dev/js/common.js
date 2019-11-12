ce4bde62a46b129bc44cbb8ccac9e306235c4b70
RING <purymaster@naver.com>
2019-11-12 18:28:35 +0900

    [20191112] 금일 작업분 반영
    입력폼, 팝업 가이드 작업

--- junior/dev/js/common.js
+++ junior/dev/js/common.js
@@ -87,6 +87,25 @@ $(function() {
 
     /******************** 슬라이드 제어 ********************/
 
+    /******************** Datepicker ********************/
+
+    $('[data-datepicker]').datepicker({
+        dateFormat: 'yy-mm-dd',
+        showMonthAfterYear: true,
+        monthNamesShort: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+        monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+        dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+        dayNames: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
+        buttonText: "선택",
+        yearSuffix: "년",
+        changeYear: true,
+        showOn: "button",
+        buttonImage: "../img/img/img_calendar.png",
+        buttonImageOnly: true,
+        buttonText: "Select date",
+        defaultDate: "-18y",
+    });
+
     /******************** 스크롤 애니메이션 정의 ********************/
 
     var move_el = $('[data-animation]'), //무빙 요소
@@ -151,4 +170,29 @@ $(function() {
                 $(this).parent().addClass('on')
             )
     });
+
+    /******************** 모달 제어 ********************/
+
+    $('[data-modal] .close').on('click', function() {
+        close_modal();
+    })
+
 });
+
+/******************** 모달, 로딩 제어 ********************/
+
+function show_modal(target) {
+    $(target).show();
+}
+
+function close_modal() {
+    $('[data-modal]').hide();
+}
+
+function show_loading() {
+    $('[data-loading]').show();
+}
+
+function hide_loading() {
+    $('[data-loading]').hide();
+}
